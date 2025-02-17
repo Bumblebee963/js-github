6c8a7600188d8f39139f3ff7d2186181190e4562
Bumblebee963 <a06mishra@gmail.com>
2025-02-17 14:40:25 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,6 @@
-console.log("Ayush");
+const accountId=12345
+let accountEmail="blablabla@gmail.com"
+let accountPW="22334455"
+accountcity="Kota"
+
+console.table([accountId,accountEmail,accountPW,accountcity])
